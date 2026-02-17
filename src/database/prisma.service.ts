842682c0bf3408d983eb1151819640e3c07e0c39
Executor-create <just4dotaalex@gmail.com>
2026-02-17 22:53:57 +0200

    feat: add PrismaService for database connection management

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,33 @@
+import {
+  Global,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Global()
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+    super({
+      log: ['query', 'error', 'warn'],
+      adapter,
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
