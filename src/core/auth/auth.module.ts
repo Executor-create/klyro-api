2f98409460d90350aa33659dd83fed2bb5e27c14
Executor-create <just4dotaalex@gmail.com>
2026-02-20 23:01:02 +0200

    feat: refactor AuthModule to use async JWT configuration and remove AuthRepository

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -2,21 +2,25 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/database/prisma.module';
-import { AuthRepository } from './auth.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { UserRepository } from 'src/modules/users/users.repository';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET || 'default_secret',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository, UserRepository],
+  providers: [AuthService, UserRepository, AuthGuard],
   exports: [AuthService],
 })
 export class AuthModule {}
