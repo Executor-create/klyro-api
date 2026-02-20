fb05bf89f8d333e93397bc835ac0184a14aeff43
Executor-create <just4dotaalex@gmail.com>
2026-02-20 22:07:58 +0200

    feat: configure JwtModule for authentication with token expiration

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -3,11 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/database/prisma.module';
 import { AuthRepository } from './auth.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { UserRepository } from 'src/modules/users/users.repository';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET || 'default_secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository],
+  providers: [AuthService, AuthRepository, UserRepository],
   exports: [AuthService],
 })
 export class AuthModule {}
