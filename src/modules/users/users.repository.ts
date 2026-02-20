8b4c0b50e81da88a5b5395209a276a0c4e8910fb
Executor-create <just4dotaalex@gmail.com>
2026-02-20 22:08:08 +0200

    feat: implement UserRepository with methods for user creation and retrieval

---
+++ src/modules/users/users.repository.ts
@@ -0,0 +1,39 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { User } from './entities/user.entity';
+import { Prisma } from 'generated/prisma/client';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(user: Prisma.UserCreateInput): Promise<User | null> {
+    return this.prisma.user.create({
+      data: user,
+    });
+  }
+
+  async findByIdOrThrow(id: string): Promise<User | null> {
+    const user = await this.prisma.user.findUniqueOrThrow({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new InternalServerErrorException('User not found');
+    }
+
+    return user;
+  }
+
+  async findByEmailOrThrow(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new InternalServerErrorException('User not found');
+    }
+
+    return user;
+  }
+}
