b72a2155f5cdd742b351ab79328ccd5aca22967c
Executor-create <just4dotaalex@gmail.com>
2026-03-02 21:29:50 +0200

    feat: add BullMQ integration for email processing in MailModule

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { mailerConfig } from 'src/config/mailer';
+import { BullModule } from '@nestjs/bullmq';
+import { MailConsumer } from './workers/mailWorker';
 
 @Module({
-  imports: [MailerModule.forRootAsync(mailerConfig)],
-  providers: [MailService],
+  imports: [
+    MailerModule.forRootAsync(mailerConfig),
+    BullModule.registerQueue({
+      name: 'email-queue',
+    }),
+  ],
+  providers: [MailService, MailConsumer],
   exports: [MailService],
 })
 export class MailModule {}
