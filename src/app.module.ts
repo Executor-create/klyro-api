3b8fddf575760c96cc59c562669e9934d4506a1c
Executor-create <just4dotaalex@gmail.com>
2026-03-05 22:46:58 +0200

    feat: add Discord module to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './database/prisma.module';
 import { BullModule } from '@nestjs/bullmq';
 import { queueConfig } from './config/queue';
+import { DiscordModule } from './integrations/discord/discord.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { queueConfig } from './config/queue';
     BullModule.forRoot(queueConfig),
     PrismaModule,
     AuthModule,
+    DiscordModule,
   ],
   controllers: [AppController],
   providers: [AppService],
