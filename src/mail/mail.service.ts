a222c355b9509b6599f77f4c1a4e27a118d325b7
Executor-create <just4dotaalex@gmail.com>
2026-03-06 22:52:26 +0200

    feat: enhance MailService with ConfigService for dynamic URL generation in password reset emails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,6 +1,7 @@
 import { ISendMailOptions } from '@nestjs-modules/mailer';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Queue } from 'bullmq';
 
 export interface SendEmailParams {
@@ -12,7 +13,46 @@ export interface SendEmailParams {
 
 @Injectable()
 export class MailService {
-  constructor(@InjectQueue('email-queue') private readonly emailQueue: Queue) {}
+  constructor(
+    @InjectQueue('email-queue') private readonly emailQueue: Queue,
+    private readonly configService: ConfigService
+  ) { }
+
+  async sendSignupEmail(to: string, username: string, otp: string): Promise<void> {
+    try {
+      const sendMailParams = {
+        to,
+        subject: 'Signup Confirmation',
+        template: 'signup-otp',
+        context: { username, otp },
+      };
+      await this.emailQueue.add('send-email', sendMailParams, {
+        attempts: 3,
+        backoff: 5000,
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to send signup email');
+    }
+  }
+
+  async sendForgotPasswordEmail(to: string, username: string, token: string): Promise<void> {
+    try {
+      const reset_url = `${this.configService.get('FRONTEND_URL')}/reset-password?token=${token}`;
+
+      const sendMailParams = {
+        to,
+        subject: 'Reset Your Password',
+        template: 'forgot-password',
+        context: { username, reset_url },
+      };
+      await this.emailQueue.add('send-email', sendMailParams, {
+        attempts: 3,
+        backoff: 5000,
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to send forgot password email');
+    }
+  }
 
   async sendEmail(params: SendEmailParams): Promise<void> {
     try {
